89f86ae57469c5b29572c0e2db78e4b4e96370a0
ErnestCodes <emmycontacts@gmail.com>
2022-06-21 23:20:33 +0100

    check winining state

--- App.js
+++ App.js
@@ -1,12 +1,95 @@
 import { StatusBar } from "expo-status-bar";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  Alert,
+} from "react-native";
 import bg from "./assets/bg.png";
 
 export default function App() {
+  const [map, setMap] = useState([
+    // defining my 2D grid/matrix
+    ["", "", ""], // 1st row
+    ["", "", ""], // 2nd row
+    ["", "", ""], // 3rd row
+  ]);
+
+  const [currentTurn, setCurrentTurn] = useState("x");
+
+  const onPress = (rowIndex, columnIndex) => {
+    console.warn("hello", rowIndex, columnIndex);
+    if (map[rowIndex][columnIndex] !== "") {
+      Alert.alert("Position already occupied");
+      return;
+    }
+
+    setMap((exisitingMap) => {
+      const updatedMap = [...exisitingMap];
+      updatedMap[rowIndex][columnIndex] = currentTurn;
+      return updatedMap;
+    });
+
+    setCurrentTurn(currentTurn == "x" ? "o" : "x");
+
+    checkWinningState();
+  };
+
+  const checkWinningState = () => {
+    // check rows
+    for (let i = 0; i < 3; i++) {
+      const isRowXWinning = map[i].every((cell) => cell == "x");
+      const isRowOWinning = map[i].every((cell) => cell == "o");
+
+      if (isRowXWinning) {
+        Alert.alert("X won. Row:", i);
+      }
+
+      if (isRowOWinning) {
+        Alert.alert("O won. Row:", i);
+      }
+    }
+    // check colums
+    for (let i = 0; i < 3; i++) {}
+    // check diagonals
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
-        <View style={styles.circle}></View>
+        <View style={styles.map}>
+          {map.map((row, rowIndex) => (
+            // Now we map for each cell in that row
+            <View style={styles.row}>
+              {row.map((cell, columnIndex) => (
+                <Pressable
+                  onPress={() => onPress(rowIndex, columnIndex)}
+                  style={styles.cell}
+                >
+                  {/* <View style={styles.circle} /> */}
+                  {cell == "o" && <View style={styles.circle} />}
+                  {cell == "x" && (
+                    <View style={styles.cross}>
+                      <View style={styles.crossLine} />
+                      <View
+                        style={[styles.crossLine, styles.crossLineReversed]}
+                      />
+                    </View>
+                  )}
+                </Pressable>
+              ))}
+            </View>
+          ))}
+
+          {/* 
+          <View style={styles.cross}>
+            <View style={styles.crossLine} />
+            <View style={[styles.crossLine, styles.crossLineReversed]} />
+          </View> */}
+        </View>
       </ImageBackground>
       <StatusBar style="auto" />
     </View>
@@ -25,12 +108,51 @@ const styles = StyleSheet.create({
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: 20,
+    paddingTop: 63,
   },
-  circle: {
+  map: {
+    width: "80%",
+    aspectRatio: 1,
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  cell: {
     width: 100,
     height: 100,
-    backgroundColor: "red",
+    flex: 1,
+  },
+  circle: {
+    flex: 1,
     borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+    borderWidth: 10,
+    borderColor: "#87E43A",
+  },
+  cross: {
+    flex: 1,
+  },
+  crossLine: {
+    position: "absolute",
+    left: "48%",
+    width: 10,
+    height: "100%",
+    borderRadius: 5,
+    backgroundColor: "#F54D62",
+    transform: [
+      {
+        rotate: "45deg",
+      },
+    ],
+  },
+  crossLineReversed: {
+    transform: [
+      {
+        rotate: "-45deg",
+      },
+    ],
   },
 });
